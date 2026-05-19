f0d241964778414a36fff0fb40fb3a542c475e5a
Abdulaziz <your-email@example.com>
2026-05-20 02:47:48 +0400

    Initial website upload

---
+++ script.js
@@ -0,0 +1,56 @@
+window.addEventListener('load', () => {
+    document.body.style.opacity = "1";
+});
+
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+
+    const updateCounter = () => {
+        const target = +counter.getAttribute('data-target');
+        const current = +counter.innerText;
+        const increment = target / 50;
+
+        if (current < target) {
+            counter.innerText = Math.ceil(current + increment);
+            setTimeout(updateCounter, 30);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCounter();
+
+});
+
+window.addEventListener('scroll', () => {
+
+    const cards = document.querySelectorAll(
+        '.item, .brand-card, .why-card, .gallery-container img, .trust-box, .stat-box'
+    );
+
+    cards.forEach(card => {
+
+        const top = card.getBoundingClientRect().top;
+
+        if (top < window.innerHeight - 100) {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }
+
+    });
+
+});
+const menuToggle = document.getElementById('menu-toggle');
+
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+});
+document.querySelectorAll('#nav-menu a')
+    .forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('active');
+        });
+    });
